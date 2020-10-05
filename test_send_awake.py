c6589f23e564133b189c9c72755bcccde7c9bf16
unlikeghost <unlikeghost@protonmail.com>
2020-10-04 18:11:12 -0600

    change

--- test_send_awake.py
+++ test_send_awake.py
@@ -9,32 +9,39 @@ info = StreamInfo('MY_ALFA_SMART', 'DATA', 5, 100, "string","myuidw43536")
 outlet = StreamOutlet(info)
 data_dir=os.path.join("data")
 print("Simulating data....")
+
+fc=[60,100]
+fr=[12,20]
+so=[95,100]
+
 print("enviando data")
+total=[]
+x=0
+name= "JESUS ALAN HERNANDEZ GALVAN"
 
-def main():
-    x=0
-    name= "JESUS ALAN HERNANDEZ GALVAN"
-    try:
-        os.remove(os.path.join(data_dir,"array.txt"))
-    except:
-        pass
-    xy=0
-    while True:
-        
-        datax=f"{datetime.now()},{randint(60,100)},{randint(12,20)},{randint(95,100)},{name}\n"
+try:
+    os.remove(os.path.join(data_dir,"array.txt"))
+except:
+    pass
+xy=0
+while True:
+    
+    datax=f"{datetime.now()},{randint(60,100)},{randint(12,20)},{randint(95,100)},{name}\n"
 
-        with open( os.path.join(data_dir,"array.txt"),"a") as f:
-            f.write(datax)
-            x+=1
-        if x==500:
-            with open( os.path.join(data_dir,"array.txt"),"r") as f:
-                totalf=f.readlines()
+    with open( os.path.join(data_dir,"array.txt"),"a") as f:
+        f.write(datax)
+        x+=1
+    if x==336:
+        with open( os.path.join(data_dir,"array.txt"),"r") as f:
+            totalf=f.readlines()
 
-            for i in totalf:
-                i=i.split(",")
-                for ix in range(0, len(i)): 
-                    i[-1] = i[-1].strip()
-                    print(i)
-                    outlet.push_sample(i)
-                    time.sleep(1)
-                    xy=xy+1
+        for i in totalf:
+            i=i.split(",")
+            for ix in range(0, len(i)): 
+                i[-1] = i[-1].strip()
+                print(i)
+            outlet.push_sample(i)
+            time.sleep(1)
+            xy=xy+1
+            if xy==230:
+                exit(0)
--- test_send_sleep.py
+++ test_send_sleep.py
@@ -10,32 +10,38 @@ outlet = StreamOutlet(info)
 data_dir=os.path.join("data")
 print("Simulating data....")
 
+fc=[60,100]
+fr=[12,20]
+so=[95,100]
 
-def main():
-    print("enviando data")
-    x=0
-    xy=0
-    name= "JESUS ALAN HERNANDEZ GALVAN"
-    try:
-        os.remove(os.path.join(data_dir,"sleep.txt"))
-    except:
-        pass
-    while True:
-        
-        datax=f"{datetime.now()},{randint(40,100)},{randint(8,20)},0,{name}\n"
+print("enviando data")
+total=[]
+x=0
+name= "JESUS ALAN HERNANDEZ GALVAN"
 
-        with open( os.path.join(data_dir,"sleep.txt"),"a") as f:
-            f.write(datax)
-        
-        if x==500:
-            with open( os.path.join(data_dir,"sleep.txt"),"r") as f:
-                totalf=f.readlines()
-            for i in totalf:
-                i=i.split(",")
-                for ix in range(0, len(i)): 
-                    i[-1] = i[-1].strip()
-                    print(i)
-                    outlet.push_sample(i)
-                    time.sleep(1)
-                    xy=xy+1
-        x=x+1
+try:
+    os.remove(os.path.join(data_dir,"sleep.txt"))
+except:
+    pass
+xy=0
+while True:
+    
+    datax=f"{datetime.now()},{randint(40,100)},{randint(8,18)},0,{name}\n"
+
+    with open( os.path.join(data_dir,"sleep.txt"),"a") as f:
+        f.write(datax)
+        x+=1
+    if x==190:
+        with open( os.path.join(data_dir,"sleep.txt"),"r") as f:
+            totalf=f.readlines()
+
+        for i in totalf:
+            i=i.split(",")
+            for ix in range(0, len(i)): 
+                i[-1] = i[-1].strip()
+                print(i)
+            outlet.push_sample(i)
+            time.sleep(1)
+            xy=xy+1
+            if xy==120:
+                exit(0)
